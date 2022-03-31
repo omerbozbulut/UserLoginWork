da2934a20c0bfd888809fdeac0f7bb0ef8ade21e
omer <omerbzblt613@gmail.com>
2022-03-31 23:39:14 +0300

    UI update

---
+++ UserLoginWork/UserLoginWork/Controller/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  UserLoginWork
+//
+//  Created by omer faruk bozbulut on 31.03.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
